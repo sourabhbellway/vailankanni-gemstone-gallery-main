6cd98bf2225f0e971c1f9c48e95837f6415f27d6
sourabh <sourabhbellway@gmail.com>
2025-09-30 18:04:24 +0530

    dashboard padding

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -96,28 +96,9 @@ const AdminDashboard = () => {
   };
 
   return (
-    <div className="">
-      <div className="px-6 ">
-        {/* Overview Cards */}
-        {/* <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 mb-8">
-          {overviewStats.map((stat, index) => (
-            <Card key={index}>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                  {stat.title}
-                </CardTitle>
-                <stat.icon className="h-4 w-4 text-muted-foreground" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">{stat.value}</div>
-                <p className="text-xs text-muted-foreground">
-                  <span className="text-green-600">{stat.change}</span> from
-                  last month
-                </p>
-              </CardContent>
-            </Card>
-          ))}
-        </div> */}
+    <div className="p-6">
+      <div className=" ">
+       
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-6">
           {/* Total Orders */}
